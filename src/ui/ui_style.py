6a3d912c76d8b2a46240013859abfb9c5080c7d9
Hentaikamenride <anvanphuchuynh@gmail.com>
2019-02-04 17:27:25 -0800

    Adding UI style class.

---
+++ src/ui/ui_style.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2018 - This notice is to be included in all relevant source files.
+# "Brandon Goldbeck" <bpg@pdx.edu>
+# “Anthony Namba” <anamba@pdx.edu>
+# “Brandon Le” <lebran@pdx.edu>
+# “Ann Peake” <peakean@pdx.edu>
+# “Sohan Tamang” <sohan@pdx.edu>
+# “An Huynh” <an35@pdx.edu>
+# “Theron Anderson” <atheron@pdx.edu>
+# This software is licensed under the MIT License. See LICENSE file for the full text.
+
+
+class UI_style():
+    """The enumerated class the stores the possible application states.
+    """
+    # Utility
+    button_background_color = ""
+    button_text_color = ""
+
+    # Main Panel
+    main_panel_background_color = ""
+
+    # Metadata Panel
+    metadata_background_color = ""
+    metadata_context_text_color = ""
+    metadata_input_text_color = ""
+    metadata_input_background = ""
+
+    # Conversion Panel
+    conversion_background_color = ""
+    # Log Panel
+    log_background_color = ""
+    log_text_background_color = ""
+    log_default_text_color = ""
+    log_info_text_color = ""
+    log_warning_text_color = ""
+    log_debug_text_color = ""
+    log_error_text_color = ""
+
+    def build_mvp(self):
+
+        pass
+
+    def build_moonshot(self):
+
+        pass
