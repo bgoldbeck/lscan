402a4ecaad7ba21163c7be740c915497f519e28e
melon <melonnelise@gmail.com>
2019-02-14 11:10:30 -0800

    added code if help/about files not found

--- src/ui/metadata_panel.py
+++ src/ui/metadata_panel.py
@@ -223,31 +223,24 @@ class MetadataPanel(wx.Panel, IUIBehavior):
         :return:
         """
         help_text = UIDriver.get_assets_file_text("HELP.txt")
-        popup = Popup(self.GetTopLevelParent(), "Help", help_text)
-        popup.Show(True)
-
-        """if help_text is not None:
-            wx.MessageBox(help_text, "Help", wx.OK | wx.ICON_QUESTION)
+        if help_text is not None:
+            popup = Popup(self.GetTopLevelParent(), "Help", help_text)
         else:
-            wx.MessageBox("Could not read help text file, sorry.", "Error",
-                          wx.OK | wx.ICON_INFORMATION)"""
-
+            popup = Popup(self.GetTopLevelParent(), "Error",
+                          "Could not read help text file, sorry.")
+        popup.Show(True)
     def about(self, event):
         """Presents program name, program version, copyright information, licensing information, and authors to user.
         :param event:
         :return:
         """
         about_text = UIDriver.get_assets_file_text("ABOUT.txt")
-        popup = Popup(self.GetTopLevelParent(), "About", about_text)
-        popup.Show(True)
-
-        """
         if about_text is not None:
-            wx.MessageBox(about_text, "About LScan", wx.OK | wx.ICON_INFORMATION)
+            popup = Popup(self.GetTopLevelParent(), "About", about_text)
         else:
-            wx.MessageBox("Could not read about text file, sorry.", "Error",
-                          wx.OK | wx.ICON_INFORMATION)
-        """
+            popup = Popup(self.GetTopLevelParent(), "Error",
+                          "Could not read about text file, sorry.")
+        popup.Show(True)
 
     def browse_input(self, event):
         """Browse for a valid STL input file.
