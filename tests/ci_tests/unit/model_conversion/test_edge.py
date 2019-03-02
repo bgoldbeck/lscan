88bc79bcf772610f65cbdada208ee905e94cb159
greenunknown <greenunknown0@gmail.com>
2019-03-02 12:25:15 -0800

    Brandon Le: Adding unit tests to edge class for dot() and same_edge() methods.

--- tests/ci_tests/unit/model_conversion/test_edge.py
+++ tests/ci_tests/unit/model_conversion/test_edge.py
@@ -18,4 +18,43 @@ class EdgeTest(unittest.TestCase):
         length = test_edge.length()
         self.assertTrue(length, math.sqrt(3))
 
+    def test_EdgeDot(self):
+        test_edge1 = Edge(0, 0, 0, 1, 1, 1)
+        test_edge2 = Edge(1, 1, 1, 0, 0, 0)
+        test_edge3 = Edge(-1, -1, -1, 0, 0, 0)
+        dot1 = Edge.dot(test_edge1, test_edge2)
+        dot2 = Edge.dot(test_edge1, test_edge3)
+        self.assertEqual(dot1, -3)
+        self.assertEqual(dot2, 3)
+
+    def test_EdgeSameEdge(self):
+        test_edge1 = Edge(0, 0, 0, 1, 1, 1)
+        test_edge2 = Edge(1, 1, 1, 0, 0, 0)
+        test_edge3 = Edge(0, 0, 0, 2, 2, 2)
+
+        same_edge1 = Edge.same_edge(test_edge1, test_edge2)
+        same_edge2 = Edge.same_edge(test_edge1, test_edge1)
+        same_edge3 = Edge.same_edge(test_edge1, test_edge3)
+        self.assertFalse(same_edge1)
+        self.assertTrue(same_edge2)
+        self.assertFalse(same_edge3)
+
+    # TODO
+    def test_AreOverlappingEdges(self):
+        test_edge1 = Edge(0, 0, 0, 1, 1, 1)
+        test_edge2 = Edge(1, 1, 1, 0, 0, 0)
+        pass
+
+    # TODO
+    def test_HasSharedVertex(self):
+        pass
+
+    # TODO
+    def test_EdgeIsParallelOrAntiparallel(self):
+        test_edge1 = Edge(0, 0, 0, 1, 1,1)
+        test_edge_parallel = Edge(1, 1, 1, 2, 2, 2)
+        test_edge_anti_parallel = Edge(2, 2, 2, 1, 1, 1)
+
+        pass
+
 
